adced92c3d60518ed00ab56c23780db60bea7a28
Rafouf69 <raphael.vignon@insa-lyon.fr>
2021-01-20 16:22:01 +0100

    stats .cpp et .h

--- src/Stats.cpp
+++ src/Stats.cpp
@@ -11,10 +11,13 @@
 //---------------------------------------------------------------- INCLUDE
 
 //-------------------------------------------------------- Include système
+#include <map>
+#include <iostream>
+#include <string> 
 
 //------------------------------------------------------ Include personnel
-
-
+#include "Stats.h" 
+using namespace std;
 //------------------------------------------------------------- Constantes
 
 //----------------------------------------------------------------- PUBLIC
@@ -25,15 +28,31 @@
 //
 //{
 //} //----- Fin de Méthode
+void Stats::generateDot(string fileName)
+{
 
+} 
 
+void Stats::generateClassement()
+{
+
+}
+
+void Stats::addGraphe(string siteCible, string siteReferent)
+{}
+
+void Stats::addOccurence(string siteCible)
+{
+
+} 
 //------------------------------------------------- Surcharge d'opérateurs
 
 
 
 //-------------------------------------------- Constructeurs - destructeur
+Stats::Stats(){} 
 
-
+Stats::~Stats(){}
 
 //------------------------------------------------------------------ PRIVE
 
--- src/Stats.h
+++ src/Stats.h
@@ -38,12 +38,22 @@ public:
     //
     // Contrat :
     //
+    void generateDot(string fileName);
 
+    void generateClassement();
+
+    void addGraphe(string siteCible, string siteReferent);
+
+    void addOccurence(string siteCible);
 //------------------------------------------------- Surcharge d'opérateurs
 
 
 //-------------------------------------------- Constructeurs - destructeur
+    Stats();
+    // Constructeur
 
+    ~Stats();
+    // Destructeur 
 //------------------------------------------------------------------ PRIVE
 
 protected:
@@ -53,8 +63,15 @@ protected:
 //----------------------------------------------------- Méthodes protégées
 
 //----------------------------------------------------- Attributs protégés
-    map<string, map<string, int>>
-  
+
+    // double map permettant de stocker le graphe, la première clé est la cible, la deuxième clé est le référent
+    map<string, map<string, int>> graphe;
+
+    // map contenant les les sites et leur nombre d'occurence, la clé est la cible
+    map<string, int> nbrOcurrence;
+
+    // multimap contenant le classement des sites en fonction de leur nombre d'occurence, la clé est le nombre d'occurence
+    multimap<int, string> classemeent;
 };
 
 //-------------------------------- Autres définitions dépendantes de <Stats>
